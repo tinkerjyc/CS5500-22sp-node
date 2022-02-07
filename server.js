79de4068638bdad8bebc626897624f4f9ac9779c
jyc <tinkerjyc@gmail.com>
2022-02-07 00:00:20 -0500

    Fix bug

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/movie-db');
 
 
-// const express = require('express');
-// const app = express();
-//
-// app.get('/hello', (req, res) =>
-//   res.send('Hello World!'));
+const express = require('express');
+const app = express();
+
+app.get('/hello', (req, res) =>
+  res.send('Hello World!'));
 
 require('./movies/service')(app);
 
