dee043a7d29c07780cc366bc5017ae88febd97f0
jyc <tinkerjyc@gmail.com>
2022-04-01 14:23:50 -0400

    Fix heroku .env

--- server.ts
+++ server.ts
@@ -5,6 +5,9 @@
  *     <li>users</li>
  *     <li>tuits</li>
  *     <li>likes</li>
+ *     <li>follows</li>
+ *     <li>bookmarks</li>
+ *     <li>messages</li>
  * </ul>
  *
  * Connects to a remote MongoDB instance hosted on the Atlas cloud database
@@ -13,38 +16,40 @@
 require('dotenv').config({
     path: "./.env"
 });
-import express, {Request, Response} from 'express';
-import CourseController from "./controllers/CourseController";
-import UserController from "./controllers/UserController";
+import express from 'express';
+import mongoose from 'mongoose';
+import UserController from './controllers/UserController';
 import TuitController from "./controllers/TuitController";
 import LikeController from "./controllers/LikeController";
+import DislikeController from "./controllers/DislikeController";
+import FollowController from "./controllers/FollowController";
+import BookmarkController from "./controllers/BookmarkController";
+import MessageController from "./controllers/MessageController";
 import SessionController from "./controllers/SessionController";
 import AuthenticationController from "./controllers/AuthenticationController";
-import mongoose from "mongoose";
 import GroupController from "./controllers/GroupController";
-import DisLikeController from "./controllers/DisLikeController";
-import FollowController from "./controllers/FollowController";
-import BookmarkController from "./controllers/BookMarkController";
-import MessageController from "./controllers/MessageController";
 
-const cors = require("cors");
+var cors = require('cors')
 const session = require("express-session");
-
-// build the connection string
-// const PROTOCOL = "mongodb+srv";
-// const DB_USERNAME = process.env.DB_USERNAME;
-// const DB_PASSWORD = process.env.DB_PASSWORD;
-// const HOST = "cluster0.m8jeh.mongodb.net";
-// const DB_NAME = "myFirstDatabase";
-// const DB_QUERY = "retryWrites=true&w=majority";
-const connectionString = `mongodb+srv://root:toor@whiteboard.quiue.mongodb.net/movie-db?retryWrites=true&w=majority`;// connect to the database
-mongoose.connect(connectionString);
-
 const app = express();
 app.use(cors({
     credentials: true,
     origin: process.env.CORS_ORIGIN
 }));
+
+// build the connection string
+const PROTOCOL = "mongodb+srv";
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const HOST = "whiteboard.quiue.mongodb.net";
+const DB_NAME = "movie-db";
+const DB_QUERY = "retryWrites=true&w=majority";
+const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
+// connect to the database
+mongoose.connect(connectionString);
+
+// connect to the database
+// mongoose.connect('mongodb://localhost:27017/tuit-db');
 
 let sess = {
     secret: process.env.EXPRESS_SESSION_SECRET,
@@ -63,24 +68,19 @@ if (process.env.NODE_ENV === 'production') {
 app.use(session(sess))
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) =>
-    res.send('Welcome!'));
-
-app.get('/add/:a/:b', (req: Request, res: Response) =>
-    res.send(req.params.a + req.params.b));
-
 // create RESTful Web service API
-const courseController = new CourseController(app);
+
 const userController = UserController.getInstance(app);
 const tuitController = TuitController.getInstance(app);
-const likesController = LikeController.getInstance(app);
-const disLikeController = DisLikeController.getInstance(app);
+const likeController = LikeController.getInstance(app);
+const dislikeController = DislikeController.getInstance(app);
 const followController = FollowController.getInstance(app);
 const bookmarkController = BookmarkController.getInstance(app);
 const messageController = MessageController.getInstance(app);
 SessionController(app);
 AuthenticationController(app);
 GroupController(app);
+
 /**
  * Start a server listening at port 4000 locally
  * but use environment variable PORT on Heroku if available.
