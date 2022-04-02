8230b7d8e7df12725551962a5f414a6190f268a3
jyc <tinkerjyc@gmail.com>
2022-04-02 15:51:20 -0400

    Fix likes&dislikes bug

--- services/AuthenticationService.ts
+++ services/AuthenticationService.ts
@@ -1,87 +1,53 @@
-import UserDao from "../daos/UserDao";
-import mongoose from "mongoose";
+/**
+ * @file Implements an authentication service. Declares the following methods:
+ * <ul>
+ *     <li>login</li>
+ *     <li>register</li>
+ * </ul>
+ */
+import axios from "axios";
+const BASE_URL = process.env.REACT_APP_BASE_URL
+const AUTH_API = `${BASE_URL}/api/auth`
+
+
+import UserDao from "../daos/UserDao"
 
 const userDao: UserDao = UserDao.getInstance();
 
-// const PROTOCOL = "mongodb+srv";
-// const DB_USERNAME = "giuseppi";//process.env.DB_USERNAME;
-// const DB_PASSWORD = "supersecretpassword";//process.env.DB_PASSWORD;
-// const HOST = "cluster0.m8jeh.mongodb.net";
-// const DB_NAME = "myFirstDatabase";
-// const DB_QUERY = "retryWrites=true&w=majority";
-const connectionString = `mongodb+srv://root:toor@whiteboard.quiue.mongodb.net/movie-db?retryWrites=true&w=majority`;
-// connect to the database
-mongoose.connect(connectionString);
 
-
+/**
+ * Check if user with the given username and password exists or not
+ * @param {string} u Represents username
+ * @param {Response} p Represents password
+ */
 export const login = (u: string, p: string) =>
-  userDao.findUserByCredentials(u, p)
-    .then(user => {
-      if (user) {
-        return user;
-      } else {
-        throw "Unknown user"
-      }
-    })
-    .then(user => user)
-    .catch(e => e)
+    userDao.findUserByCredentials(u, p)
+        .then(user => {
+            if (user) {
+                return user;
+            } else {
+                throw "Unknown user"
+            }
+        })
+        .then(user => user)
+        .catch(e => e)
 
+/**
+ * Register user with the given username and password if the same username does not exist
+ * @param {string} u Represents username
+ * @param {Response} p Represents password
+ * @param {string} e Represents email
+ */
 export const register = (u: string, p: string, e: string) =>
-  userDao.findUserByUsername(u)
-    .then(user => {
-      if (user) {
-        throw 'User already exists';
-      } else {
-        return userDao.createUser({
-          username: u, password: p, email: e
-        });
-      }
-    })
-    .then(newUser => newUser)
-    .catch(e => e);
-
-export const initializeSalaries = (salary: number) => {
-  return userDao.findAllUsers()
-    .then(users => {
-      const sPromises = users.map(user =>
-        userDao.updateUserSalaryByUsername(user.username, salary));
-      const resultPromise = Promise.all(sPromises);
-      resultPromise
-        .then(values => {
-          return values
+    userDao.findUserByUsername(u)
+        .then(user => {
+            if (user) {
+                throw 'User already exists';
+            } else {
+                return userDao.createUser({
+                    username: u, password: p, email: e
+                });
+            }
         })
-    })
-}
-
-export const giveRaise = (raise: number) => {
-  return userDao.findAllUsers()
-    .then(users => {
-      const salaryPromises = users.map(user => {
-        // @ts-ignore
-        const newSalary = user.salary * (1 + raise / 100);
-        return userDao.updateUserSalaryByUsername(
-          user.username,
-          newSalary)
-      });
-      const resultPromise = Promise.all(salaryPromises);
-      resultPromise
-        .then(values => {
-          return values;
-        })
-    })
-}
-
-// giveRaise(50)
-//   .then(results => console.log(results));
-//
-// initializeSalaries(50000)
-//   .then(results => console.log(results));
-//
-// register('alice008', 'alice234', 'alice234@gmail.com')
-//   .then(user => console.log(user))
-//
-login('alice008', 'alice234')
-  .then(user => console.log(user))
-
-// userDao.findAllUsers()
-//   .then(users => console.log(users));
+        .then(newUser => newUser)
+        .catch(e => e);
